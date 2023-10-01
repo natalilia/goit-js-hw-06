fea4663faf3078b10d69a56e79a617e1111daac4
natalilia <nata.semenii@gmail.com>
2023-10-01 21:30:09 +0200

    task 5

---
+++ src/src/js/task-05.js
@@ -0,0 +1,10 @@
+"use strict";
+const textInput = document.querySelector("#name-input");
+const textOutput = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+  textOutput.textContent = event.currentTarget.value;
+  if(event.currentTarget.value.trim() === "") {
+    textOutput.textContent = "Anonymous";
+  }
+});
