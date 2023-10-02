3e6d6a7957c7e5202779dd54220860b028a28dc4
natalilia <nata.semenii@gmail.com>
2023-10-02 21:21:11 +0200

    task 8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,22 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+    
+  const result = {
+    login: email,
+    password,
+  };
+  
+  console.log(result);
+  form.reset();
+}
--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector("button.change-color");
+const body = document.body;
+const bodyColor = document.querySelector(".color");
+
+btn.addEventListener("click", (event) => {
+  const currentColor = getRandomHexColor();
+  body.style.backgroundColor = currentColor;
+  bodyColor.textContent = currentColor;
+});
